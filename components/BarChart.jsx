48c08e0b15f6dc372829eeff2bd15d2fa5a9bed6
Akansha Aggarwal <akanshaaggarwal9876@gmail.com>
2023-09-22 18:28:45 +0530

    Updated BarChart

--- components/BarChart.jsx
+++ components/BarChart.jsx
@@ -1,15 +1,33 @@
-
-import { Bar } from "react-chartjs-2";
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import Chart from 'chart.js/auto'; // Import Chart from 'chart.js/auto'
 
 const BarChart = () => {
+  const data = {
+    labels: ["", "Week 1", "", "Week 2", "", "Week 3", "", "Week 4", ""],
+    datasets: [
+      {
+        label: "User",
+        data: [0, 480, 0, 420, 0, 200, 0, 380, 0],
+        backgroundColor: "#98D89E",
+        borderRadius: 3,
+      },
+      {
+        label: "Guest",
+        data: [0, 390, 0, 370, 0, 300, 0, 320, 0],
+        backgroundColor: "#EE8484",
+        borderRadius: 3,
+      },
+    ],
+  };
+
   const options = {
     responsive: true,
     maintainAspectRatio: false,
-   
     plugins: {
       legend: {
-        position: "top",
-        align: "end",
+        position: 'top',
+        align: 'end',
       },
     },
     scales: {
@@ -21,10 +39,10 @@ const BarChart = () => {
         },
       },
       x: {
+        type: 'category', // Use the 'category' scale
         grid: {
           display: false,
         },
-       
       },
     },
   };
@@ -34,7 +52,7 @@ const BarChart = () => {
       id="mainGraphContainer"
       className="p-5 rounded-[20px] h-[370px] bg-white w-[100%] mb-auto"
     >
-      <div className=" max-w-[99%] h-[300px] m-auto mb-3">
+      <div className="max-w-[99%] h-[300px] m-auto mb-3">
         <div className="flex justify-between items-center">
           <div className="flex-[7]">
             <h3 className="font-bold text-2xl">Activities</h3>
@@ -43,29 +61,7 @@ const BarChart = () => {
             </div>
           </div>
         </div>
-        <Bar
-          datasetIdKey="id"
-          data={{
-            labels: ["","Week 1","", "Week 2","", "Week 3","", "Week 4",""],
-            datasets: [
-              {
-                id: 1,
-                label: "User",
-                data: [0,480,0, 420,0, 200,0, 380,0],
-                backgroundColor: "#98D89E",
-                borderRadius: 3,
-              },
-              {
-                id: 2,
-                label: "Guest",
-                data: [0,390,0, 370,0, 300, 0,320,0],
-                backgroundColor: "#EE8484",
-                borderRadius: 3,
-              },
-            ],
-          }}
-          options={options}
-        />
+        <Bar data={data} options={options} />
       </div>
     </div>
   );
